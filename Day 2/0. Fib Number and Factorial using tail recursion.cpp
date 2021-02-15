b788b995afd7d98bab20440bf8a33e32529b9002
prajjwalgeek <prajjwalgeek@gmail.com>
2021-02-15 14:43:38 +0530

    Day 2 0

---
+++ Day 2/0. Fib Number and Factorial using tail recursion.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+
+using namespace std;
+ 
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+ 
+int main ()
+{
+    int n = 9;
+    cout << fib(n);
+    getchar();
+    return 0;
+}
