ff8b519056c1249b84b4d122f09c7c184da1937c
Prajjwal Mishra <43957680+prajjwalgeek@users.noreply.github.com>
2021-02-21 11:23:42 +0530

    Done Day4 Problems

---
+++ Day 4/01-BalancedPara.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool validate(string str)
+{
+    stack<char> s;
+    int n = str.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+        {
+            s.push(str[i]);
+        }
+        else
+        {
+            if (s.empty() == true)
+            {
+                return false;
+            }
+            char c = s.top();
+            s.pop();
+            if (c == '(')
+            {
+                if (c == ')')
+                {
+                    return true;
+                }
+            }
+            if (c == '{')
+            {
+                if (c == '}')
+                {
+                    return true;
+                }
+            }
+            if (c == '[')
+            {
+                if (c == ']')
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return (s.empty());
+}
+
+int main()
+{
+    string s = "{()}[]";
+    string s1 = "{{})]";
+    cout << validate(s) << " ";
+    cout << validate(s1);
+}
